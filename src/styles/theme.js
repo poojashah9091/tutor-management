147a3d38045849a1bd815313233e17f06fce0846
Pooja Shah <poojashah9091@gmail.com>
2023-06-22 17:52:51 -0400

    Basic config files

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: '#ff5630',
+     },
+  },
+});
+
+export default theme;
