bf7b2de86b62902cde81815c53a10ca3cfdab7ba
Pooja Shah <poojashah9091@gmail.com>
2023-06-26 23:27:40 -0400

    Styling related fixes

--- src/pages/tutor/[tutorId].js
+++ src/pages/tutor/[tutorId].js
@@ -4,20 +4,20 @@ import ProfessionalDetails from "@/components/tutorDetails/ProfessionalDetails";
 import PersonalDetails from "@/components/tutorDetails/PersonalDetails";
 import ManagerDetails from "@/components/tutorDetails/ManagerDetails";
 
-const tutorDetails = ({tutorData}) =>{
+const tutorDetails = ({tutorData}) =>{ 
     return(
         <main className={styles.tutor_details_container}>
             <article className={styles.tutor_details_section}>
-                <Paper elevation={3}>
+                <Paper elevation={3} sx={{overflow: 'auto'}}>
                 <PersonalDetails data={tutorData.personal} empId={tutorData.professional.empId}/>
                 </Paper>
-                <Paper elevation={3}>
+                <Paper elevation={3} sx={{overflow: 'auto'}}>
                     <ProfessionalDetails data={tutorData.professional} />
                 
                 </Paper>
             </article>
             <article className={styles.manager_details_section}>
-                <Paper elevation={3}>
+                <Paper elevation={3} sx={{overflow: 'auto'}}>
                     <ManagerDetails data={tutorData.professional.managerDetails} />
                 </Paper>
             </article>
