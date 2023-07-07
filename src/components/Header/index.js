e0164b8cd79cf99f639c9746977b4dadd426d2bb
Pooja Shah <poojashah9091@gmail.com>
2023-07-06 20:31:01 -0400

    Updation of Header component to include the SignOut component as well

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "../../../public/logo.png";
+import logo from "../../../public/logo_light.png";
+import SignOutButton from "../signOut";
 
-const Header = () =>{
+const Header = () => {
     return (
         <nav className='header'>
             <Link href="/tutor">
-                <Image src={logo} alt="Logo" width={150}/>
+                <Image src={logo} alt="Logo" width={150} />
             </Link>
+            <SignOutButton />
         </nav>
     )
 }
-  
+
 export default Header;
